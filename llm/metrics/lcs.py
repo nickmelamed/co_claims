0352d1cf87135bb92736203148298ba2a9af4704
Nick Melamed <nickmelamed@berkeley.edu>
2026-03-29 16:59:53 -0700

    fixed LCS missing upload

---
+++ llm/metrics/lcs.py
@@ -0,0 +1,33 @@
+from .utils import extract_json
+
+LCS_PROMPT = """
+Evaluate the logical consistency of the claim.
+
+Claim:
+{claim}
+
+Instructions:
+- Break the claim into sub-statements if needed
+- Identify contradictions or inconsistencies within the claim
+- Consider implicit contradictions
+
+Score from 0 to 1:
+1 = fully logically consistent
+0 = internally contradictory
+
+Output:
+<json>
+{{"score": float}}
+</json>
+"""
+
+
+class LCS_LLM:
+    def __init__(self, ensemble):
+        self.ensemble = ensemble
+
+    def score(self, claim):
+        result = self.ensemble.evaluate(
+            LCS_PROMPT.format(claim=claim)
+        )
+        return result.get("entailment_mean", 1.0)
