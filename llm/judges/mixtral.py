d5bf983fb7575caa1414c43aecdae49fcffe7dff
Nick Melamed <nickmelamed@berkeley.edu>
2026-03-29 00:47:52 -0700

    adding judge scripts

---
+++ llm/judges/mixtral.py
@@ -0,0 +1,19 @@
+class MixtralJudge(BaseJudge):
+    def __init__(self, client):
+        self.client = client
+
+    def evaluate(self, prompt):
+        response = self.client.chat.completions.create(
+            model="mistralai/Mixtral-8x7B-v0.1",
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0
+        )
+        return self._parse(response)
+
+    def _parse(self, response):
+        import json
+        try:
+            text = response.choices[0].message.content
+            return json.loads(text.split("<json>")[-1])
+        except:
+            return {"error": True}
